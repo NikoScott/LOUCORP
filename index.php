a7ac617f51abc3bcdbad821db738087408aa42bf
Nicolas PEREIRE <nico63@live.fr>
2024-03-25 21:13:12 +0100

    Update index.php

--- index.php
+++ index.php
@@ -14,13 +14,15 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
     $message = $_POST['message'];
 
     // Vérifier si tous les champs sont saisis
-    if (!empty($nom) 
-    && !empty($prenom) 
-    && !empty($email) 
-    && !empty($phone) 
-    && !empty($message) 
-    && !empty($etage) 
-    && !empty($superficie)) {
+    if (
+        !empty($nom)
+        && !empty($prenom)
+        && !empty($email)
+        && !empty($phone)
+        && !empty($message)
+        && !empty($etage)
+        && !empty($superficie)
+    ) {
         // Vérifier si l'adresse e-mail est valide
         if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
             // Validation CAPTCHA côté serveur
@@ -33,7 +35,7 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
             );
 
             $options = array(
-                'http' => array (
+                'http' => array(
                     'method' => 'POST',
                     'header' => 'Content-Type: application/x-www-form-urlencoded',
                     'content' => http_build_query($recaptchaData)
@@ -46,56 +48,56 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
             if ($captchaSuccess->success) {
                 // CAPTCHA validé avec succès, envoyer l'e-mail
 
-            // Adresse e-mail de destination
-            $to = "loucorp.ed@gmail.com";
-       
-            // Sujet du message
-            $subject = "Nouveau message de LOUCORP.fr";
-            // Corps du message
-            $body = "   Prenom: $prenom\n
+                // Adresse e-mail de destination
+                $to = "loucorp.ed@gmail.com";
+
+                // Sujet du message
+                $subject = "Nouveau message de LOUCORP.fr";
+                // Corps du message
+                $body = "   Prenom: $prenom\n
                         Nom: $nom\n
                         Email: $email\n
                         Téléphone: $phone\n
                         Etage: $etage\n
                         Superficie: $superficie\n
                         Message: $message";
-            // En-têtes du message
-            $headers = "From: $email";
+                // En-têtes du message
+                $headers = "From: $email";
 
-            // Envoyer l'e-mail
-            if (mail($to, $subject, $body, $headers)) {
-                $confirmationMessage = "Merci $prenom $nom! <br>
+                // Envoyer l'e-mail
+                if (mail($to, $subject, $body, $headers)) {
+                    $confirmationMessage = "Merci $prenom $nom! <br>
                 Votre message a été envoyé avec succès.<br><br>
                 LOUCORP vous contactera ou vous enverra votre devis rapidement";
-                $confirmationClass = "success";
+                    $confirmationClass = "success";
+                } else {
+                    $confirmationMessage = "Une erreur s'est produite lors de l'envoi de votre message. Veuillez réessayer svp.";
+                    $confirmationClass = "warning";
+                }
             } else {
-                $confirmationMessage = "Une erreur s'est produite lors de l'envoi de votre message. Veuillez réessayer svp.";
-                $confirmationClass = "warning";
+                // L'adresse e-mail n'est pas valide
+                $confirmationMessage = "Veuillez saisir une adresse e-mail valide.";
+                $confirmationClass = "danger";
             }
         } else {
-            // L'adresse e-mail n'est pas valide
-            $confirmationMessage = "Veuillez saisir une adresse e-mail valide.";
+            // Tous les champs ne sont pas saisis
+            $confirmationMessage = "Veuillez remplir tous les champs du formulaire.";
             $confirmationClass = "danger";
         }
-    } else {
-        // Tous les champs ne sont pas saisis
-        $confirmationMessage = "Veuillez remplir tous les champs du formulaire.";
-        $confirmationClass = "danger";
     }
 }
 ?>
 
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta name="description"
-        content="Votre partenaire de confiance pour des travaux de plaquisterie, plâtrerie et stafferie à Nice et dans les Alpes-Maritimes. Qualité, réactivité et mobilité garanties. Contactez-nous dès aujourd'hui pour concrétiser vos projets." />
+    <meta name="description" content="Votre partenaire de confiance pour des travaux de plaquisterie, plâtrerie et stafferie à Nice et dans les Alpes-Maritimes. Qualité, réactivité et mobilité garanties. Contactez-nous dès aujourd'hui pour concrétiser vos projets." />
     <title>LOUCORP</title>
     <meta name="robots" content="index, follow" />
-    <meta name="keywords"
-        content="plaquisterie, plâtrerie, stafferie, Nice, Alpes-Maritimes, rénovation, construction, devis rapide, travail de qualité" />
+    <meta name="keywords" content="plaquisterie, plâtrerie, stafferie, Nice, Alpes-Maritimes, rénovation, construction, devis rapide, travail de qualité" />
     <meta name="author" content="DevWebConcept" />
     <meta http-equiv="Content-Language" content="fr">
     <!-- Favicon-->
@@ -114,9 +116,7 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
     <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
         <div class="container px-4 px-lg-5">
             <a href="#"><img src="assets/img/logo.png" alt="Logo de l'entreprise LOUCORP" style="height: 60px;"></a>
-            <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
-                aria-label="Toggle navigation">
+            <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarResponsive">
@@ -140,30 +140,30 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
             </div>
         </div>
     </nav>
-<!-- Home -->
-<div id="home" class="d-flex flex-column mb-4 mx-auto justify-content-center">
-    <div id="home-text" class="position-relative">
-        <h1 class="text-center mb-2 fade-in" style="font-size: 120px">LOUCORP</h1>
-        <h2 class="text-center fs-3 mb-4"> INNOVATION BATIMENT </h2>
-        <h3 class="text-center mt-2 mb-4 fs-4 fst-italic "> Innovons Ensemble !</h3>
-        <div class="container mt-4 mb-4">
-            <div class="row justify-content-center">
-                <div class="text-center">
-                    <div class="scrolling-container">
-                        <div class="scrolling-text">
-                            <span class="px-3 mobile">FAUX PLAFONDS</span>
-                            <span class="px-3 mobile">CLOISONS</span>
-                            <span class="px-3 mobile">HABILLAGE</span>
-                            <span class="px-3 mobile">ENDUITS</span>
-                            <span class="px-3 mobile">ISOLATION</span>
+    <!-- Home -->
+    <div id="home" class="d-flex flex-column mb-4 mx-auto justify-content-center">
+        <div id="home-text" class="position-relative">
+            <h1 class="text-center mb-2 fade-in" style="font-size: 120px">LOUCORP</h1>
+            <h2 class="text-center fs-3 mb-4"> INNOVATION BATIMENT </h2>
+            <h3 class="text-center mt-2 mb-4 fs-4 fst-italic "> Innovons Ensemble !</h3>
+            <div class="container mt-4 mb-4">
+                <div class="row justify-content-center">
+                    <div class="text-center">
+                        <div class="scrolling-container">
+                            <div class="scrolling-text">
+                                <span class="px-3 mobile">FAUX PLAFONDS</span>
+                                <span class="px-3 mobile">CLOISONS</span>
+                                <span class="px-3 mobile">HABILLAGE</span>
+                                <span class="px-3 mobile">ENDUITS</span>
+                                <span class="px-3 mobile">ISOLATION</span>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+        <a class="mt-4 mx-auto btn btn-bordeau text-white shadow-xl" href="#contact">Obtenez votre devis rapidement !</a>
     </div>
-    <a class="mt-4 mx-auto btn btn-bordeau text-white shadow-xl" href="#contact">Obtenez votre devis rapidement !</a>
-</div>
 
     <!-- About -->
     <div id="about" class="text-light-emphasis text-center">
@@ -266,7 +266,7 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
             <img class="img-size p-1 slide-in" src="assets/img/IMG_3865.JPG" alt="photo de realisation">
             <img class="img-size p-1 slide-in" src="assets/img/IMG_6605.jpeg" alt="photo de realisation">
             <img class="img-size p-1 slide-in" src="assets/img/IMG_6607.jpeg" alt="photo de realisation">
-            
+
         </div>
     </div>
     <hr class="separator col-8 mx-auto" style="height:4px;background:#7f0000;">
@@ -277,18 +277,17 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
             <div class="col-lg-6">
                 <div class="container mt-2">
 
-                <!-- CONFIRMATION -->
-                <?php if(!empty($confirmationClass)) { ?>
                     <!-- CONFIRMATION -->
-                    <div id="msg" class="text-center alert alert-<?php echo $confirmationClass;?>" role="alert"><?php echo $confirmationMessage; ?></div>
-                <?php } ?>
+                    <?php if (!empty($confirmationClass)) { ?>
+                        <!-- CONFIRMATION -->
+                        <div id="msg" class="text-center alert alert-<?php echo $confirmationClass; ?>" role="alert"><?php echo $confirmationMessage; ?></div>
+                    <?php } ?>
 
                     <form method="POST" action="">
                         <div class="d-flex mb-3">
                             <div class="form-group col-md-6 pe-1">
                                 <label for="prenom">Prénom :</label>
-                                <input type="text" placeholder="Lou" class="form-control" id="prenom" name="prenom"
-                                    required />
+                                <input type="text" placeholder="Lou" class="form-control" id="prenom" name="prenom" required />
                             </div>
                             <div class="form-group col-md-6 ps-1">
                                 <label for="nom">NOM :</label>
@@ -301,23 +300,21 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
                         </div>
                         <div class="form-group mb-4">
                             <label for="phone">Téléphone :</label>
-                            <input type="tel" placeholder="06 12 34 56 78" class="form-control" id="phone" name="phone" required/>
+                            <input type="tel" placeholder="06 12 34 56 78" class="form-control" id="phone" name="phone" required />
                         </div>
                         <div class="d-flex mb-3">
                             <div class="form-group col-md-6 pe-1">
                                 <label for="etage">Etage :</label>
-                                <input type="number" placeholder="ex: 1" class="form-control" id="etage" name="etage" required/>
+                                <input type="number" placeholder="ex: 1" class="form-control" id="etage" name="etage" required />
                             </div>
                             <div class="form-group col-md-6 ps-1">
                                 <label for="superficie">Superficie :</label>
-                                <input type="number" placeholder="en m²" class="form-control" id="superficie"
-                                    name="superficie" required/>
+                                <input type="number" placeholder="en m²" class="form-control" id="superficie" name="superficie" required />
                             </div>
                         </div>
                         <div class="form-group mb-4">
                             <label for="message">Message :</label>
-                            <textarea class="form-control" placeholder="Détaillez votre demande au maximum" id="message"
-                                name="message" rows="4" required></textarea>
+                            <textarea class="form-control" placeholder="Détaillez votre demande au maximum" id="message" name="message" rows="4" required></textarea>
                         </div>
                         <div class="text-center">
                             <div class="g-recaptcha d-flex justify-content-center" data-sitekey="6LdIEaApAAAAAMuZcVbHrCkgRavhlnPNdBN5Ds32"></div>
@@ -344,11 +341,11 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
             <div class="text-center text-muted">
                 Copyright &copy; 2022 - S.A.R.L LOUCORP
             </div>
-            <div class="small text-center text-muted "> Site réalisé par <a href="https://devwebconcept.com"
-                    class="text-muted">DevWebConcept</a></div>
+            <div class="small text-center text-muted "> Site réalisé par <a href="https://devwebconcept.com" class="text-muted">DevWebConcept</a></div>
         </div>
     </footer>
     <script src="bootstrap-5.3.3/js/bootstrap.bundle.min.js"> </script>
     <script src="assets/js/script.js"></script>
 </body>
+
 </html>
